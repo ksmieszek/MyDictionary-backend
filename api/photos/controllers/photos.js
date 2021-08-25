bfa912f83139f73c2b4060ac7e72e74854795c1d
pyszaleq <buc704@gmail.com>
2021-08-25 23:57:48 +0200

    Uploading project

---
+++ api/photos/controllers/photos.js
@@ -0,0 +1,14 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async find(ctx) {
+    let entity = await strapi.services.photos.find(ctx.request.body);
+    return sanitizeEntity(entity, { model: strapi.models.photos });
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f042ca59-1239-4644-9962-626bcdc122d1'
+};
